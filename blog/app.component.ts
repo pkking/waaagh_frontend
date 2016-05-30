fdc6fb9dc1ab4bfe8fae553826f85c72874a9ceb
unknown <>
2016-05-30 14:14:49 +0800

    edit

--- blog/app.component.ts
+++ blog/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import {blogService}  from './blog.service';
 
 @Component({
   selector: 'sidebar',
   templateUrl: 'blog/sidebar.html',
-  directives: [],
+  directives: [blogService],
   providers:[],
 })
 export class sideBar{
-    
+    constructor(private blogService: blogService) { }
 };
