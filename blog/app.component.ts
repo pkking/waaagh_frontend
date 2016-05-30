64f74b3cc52dbc5573b3d79ab9e082d2773d623c
unknown <>
2016-05-30 14:13:35 +0800

    init

---
+++ blog/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidebar',
+  templateUrl: 'blog/sidebar.html',
+  directives: [],
+  providers:[],
+})
+export class sideBar{
+    
+};
